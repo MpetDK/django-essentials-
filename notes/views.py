ee763751a8cc2a57d1e410187aedb0eb3499015c
MPetDK <mahdi-ap@protonmail.com>
2022-05-23 21:27:41 +0200

    inital commit

---
+++ notes/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Notes
+from django.http import Http404
+
+# Create your views here.
+def list(request):
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+       raise Http404('Noten findes ikke') 
+
+    return render(request, 'notes/notes_detail.html', {'note': note})
