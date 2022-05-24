528a2d316e07e58262a55817175cdd08619d7867
MPetDK <mahdi-ap@protonmail.com>
2022-05-24 21:10:02 +0200

    Adds a generic listView as a replacement changes to urls.py. new reference to NotesListView

--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Notes
 from django.http import Http404
+from django.views.generic import ListView
+from .models import Notes
 
-# Create your views here.
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
 
 
 def detail(request, pk):
