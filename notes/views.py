76dae431cffcd2c526781c7dc6b93e087de16f84
MPetDK <mahdi-ap@protonmail.com>
2022-05-24 21:30:57 +0200

    Replaces the custom NoteDetail Class with a generic DetailView

--- notes/views.py
+++ notes/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
 from .models import Notes
 from django.http import Http404
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from .models import Notes
-
 
 class NotesListView(ListView):
     model = Notes
     context_object_name = "notes"
     template_name = 'notes/notes_list.html'
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-       raise Http404('Noten findes ikke') 
-
-    return render(request, 'notes/notes_detail.html', {'note': note})
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
